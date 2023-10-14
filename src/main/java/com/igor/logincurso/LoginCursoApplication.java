426807c9d1394bfac23ddf971e1cddf4057e6381
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2023-10-14 14:10:16 -0300

    first commit

---
+++ src/main/java/com/igor/logincurso/LoginCursoApplication.java
@@ -0,0 +1,13 @@
+package com.igor.logincurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/logincurso/LoginCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.logincurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
