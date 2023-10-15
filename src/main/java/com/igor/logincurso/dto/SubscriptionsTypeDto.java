e27abe2d38e10ec6284529d985a4e3cd3bf1f9b3
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2023-10-15 19:36:29 -0300

    feat: criando a validação de SubscriptionsType

--- src/main/java/com/igor/logincurso/dto/SubscriptionsTypeDto.java
+++ src/main/java/com/igor/logincurso/dto/SubscriptionsTypeDto.java
@@ -1,12 +1,26 @@
 package com.igor.logincurso.dto;
 
 import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 public class SubscriptionsTypeDto {
+
+    @NotBlank
     private String name;
+
+    @Max(value = 12)
     private Long accessMonths;
+
+    @NotNull
     private BigDecimal price;
+
+    @NotBlank
+    @Size(min = 5,max = 15)
     private String productKey;
 }
